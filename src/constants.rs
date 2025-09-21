00c1683586b2b601ab14794f5a539baf1b973a51
BIIN <116185629+BeamDC@users.noreply.github.com>
2025-09-21 12:58:59 -0400

    Define unit range constants in constants.rs
    Added constants for unit ranges in StarCraft II.

---
+++ src/constants.rs
@@ -0,0 +1,32 @@
+/// https://liquipedia.net/starcraft2/Range
+/// Unit Ranges - No Upgrades
+pub const RANGE_SCV: f32                           = 0.1;
+pub const RANGE_HELLBAT: f32                       = 2.0;
+pub const RANGE_REAPER: f32                        = 4.5;
+pub const RANGE_WIDOW_MINE: f32                    = 5.0;
+pub const RANGE_MARINE: f32                        = 5.0;
+pub const RANGE_HELLION: f32                       = 5.0;
+pub const RANGE_LIBERATOR_FIGHTER: f32             = 5.0;
+pub const RANGE_AUTO_TURRET_NO_UPGRADE: f32        = 6.0;
+pub const RANGE_BANSHEE: f32                       = 6.0;
+pub const RANGE_BATTLECRUISER: f32                 = 6.0;
+pub const RANGE_CYCLONE: f32                       = 6.0;
+pub const RANGE_GHOST: f32                         = 6.0;
+pub const RANGE_MARAUDER: f32                      = 6.0;
+pub const RANGE_VIKING_ASSAULT: f32                = 6.0;
+pub const RANGE_PLANETARY_NO_UPGRADE: f32          = 6.0;
+pub const RANGE_MISSILE_TURRET_NO_UPGRADE: f32     = 7.0;
+pub const RANGE_SIEGE_TANK_TANK: f32               = 7.0;
+pub const RANGE_THOR_GROUND: f32                   = 7.0;
+pub const RANGE_VIKING_FIGHTER: f32                = 9.0;
+pub const RANGE_LIBERATOR_DEFENDER_NO_UPGRADE: f32 = 10.0;
+pub const RANGE_THOR_AIR_EXPLOSIVE: f32            = 10.0;
+pub const RANGE_THOR_AIR_HIGH_IMPACT: f32          = 11.0;
+pub const RANGE_SIEGE_TANK_SIEGE: f32              = 13.0;
+
+
+/// Unit Ranges - Upgrades
+pub const RANGE_AUTO_TURRET_UPGRADE: f32           = 7.0;
+pub const RANGE_PLANETARY_UPGRADE: f32             = 7.0;
+pub const RANGE_MISSILE_TURRET_UPGRADE: f32        = 8.0;
+pub const RANGE_LIBERATOR_DEFENDER_UPGRADE: f32    = 12.0;
